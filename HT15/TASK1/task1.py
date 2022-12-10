4c24351adf73931d37037db3414e9258a7cb6ff8
Yuliia <yutadv@gmail.com>
2022-12-11 00:05:51 +0200

    ht15okok

--- HT15/TASK1/task1.py
+++ HT15/TASK1/task1.py
@@ -38,7 +38,20 @@ class Domains:
 			'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
 
 		return {'User-Agent': choice(desktop_agents),
-				'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+				'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+				'accept-encoding': 'gzip, deflate, br',
+				'accept-language': 'uk - UA, uk; q = 0.9, en - US; q = 0.8, en;q = 0.7',
+				'cache-control': 'no - cache',
+				'pragma': 'no - cache',
+				'sec-ch-ua': '"Google Chrome"; v = "107", "Chromium"; v = "107", "Not=A?Brand"; v = "24"',
+				'sec-ch-ua-mobile': '?0',
+				'sec-ch-ua-platform': '"macOS"',
+				'sec-fetch-dest': 'document',
+				'sec-fetch-mode': 'navigate',
+				'sec-fetch-site': 'same - origin',
+				'sec-fetch-user': '?1',
+				'upgrade-insecure-requests': '1'
+			}
 
 	def get_information(self, number_page):
 		"""Запит всієї інформації з сайту"""
